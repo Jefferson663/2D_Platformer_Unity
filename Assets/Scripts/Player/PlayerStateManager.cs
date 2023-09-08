725de6ea6cc76a128f4637c7ef32697629f10e98
Jefferson663 <143879177+Jefferson663@users.noreply.github.com>
2023-09-08 15:59:04 -0300

    Double Jump

--- Assets/Scripts/Player/PlayerStateManager.cs
+++ Assets/Scripts/Player/PlayerStateManager.cs
@@ -12,10 +12,10 @@ public class PlayerStateManager : MonoBehaviour
 
     [Header("Movement")]
     [Space]
-    public float velocity = 1.0f;
-    public float velocityJumping = 0.7f;
+    public float velocity;
+    [Range(0f,1f)]public float velocityJumping = 0.7f;
     public float jumpingPower = 5f;
-    public float abruptStop = 0.4f;
+    [Range(0f,1f)]public float abruptStop = 0.4f;
 
     [HideInInspector] public Rigidbody2D rb;
 
