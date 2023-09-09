03f77a4ddf347462538fdb1eaf210e6fe9cc8bf1
Jefferson663 <143879177+Jefferson663@users.noreply.github.com>
2023-09-09 10:29:31 -0300

    reusability changes

--- Assets/Scripts/Enemies/BasicEnemy.cs
+++ Assets/Scripts/Enemies/BasicEnemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BasicEnemy : MonoBehaviour
 {
-    [SerializeField] protected float runSpeed;
+    [SerializeField] protected float runSpeed = 2;
     [SerializeField] [Range(-1,1)] protected int direction = -1;
     protected SpriteRenderer sprite;
 
@@ -16,16 +16,16 @@ public class BasicEnemy : MonoBehaviour
             CheckDirection();
         }
     }
-    private void Update()
+    protected void Update()
     {
         transform.position += new Vector3(direction,0) * runSpeed * Time.deltaTime;
     }
-    public void OnCollisionEnter2D(Collision2D collision)
+    protected void OnCollisionEnter2D(Collision2D collision)
     {
         HandleCollision(collision);
     }
 
-    public virtual void HandleCollision(Collision2D collision)
+    protected virtual void HandleCollision(Collision2D collision)
     {
         if(collision.collider.CompareTag("wall"))
         {
